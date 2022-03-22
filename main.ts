6d37c686ec863659fdf523739cc36652122c3de6
dantef12 <100299384+dantef12@users.noreply.github.com>
2022-03-22 13:08:43 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    number += 1
+    basic.showNumber(number)
+})
+input.onButtonPressed(Button.AB, function () {
+    if (number > 0) {
+        basic.showString("P")
+    } else if (number < 0) {
+        basic.showString("N")
+    } else {
+        basic.showString("Z")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    number += -1
+    basic.showNumber(number)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (number % 2 == 2 % 2) {
+        basic.showString("even")
+    } else {
+        basic.showString("odd")
+    }
+})
+let number = 0
+number = 0
+basic.showNumber(number)
 basic.forever(function () {
 	
 })
